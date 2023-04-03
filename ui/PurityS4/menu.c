e67a707f2eb25fd80cfd0dfe116b6dd9589e85f4
Yanis Coper <yanis.coper@epita.fr>
2023-04-03 14:51:12 +0200

    envoieeee

--- ui/PurityS4/menu.c
+++ ui/PurityS4/menu.c
@@ -3,7 +3,7 @@
 #include <signal.h>
 #include <unistd.h>
 #include <string.h>
-//#include <gtk/gtk.h>
+#include <gtk/gtk.h>
 #include <math.h>
 #include <ctype.h>
 
@@ -14,7 +14,7 @@ GtkWidget  *fixed1;
 GtkWidget  *button1;
 GtkWidget  *label1;
 GtkBuilder *builder;
-
+GtkImage  *image1;
 
 int main(int argc,char *argv[])
 {
@@ -36,7 +36,7 @@ int main(int argc,char *argv[])
 	return EXIT_SUCCESS;
 }
 
-void on_button1_clicked(GtkButton *b)
+void on_button1_clicked(GtkButton *b, gpointer *userdata)
 {
 	gtk_label_set_text(GTK_LABEL(label1),(const gchar*) "Hello World");
 }
