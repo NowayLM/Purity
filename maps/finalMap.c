69f6ce52212a359836d300fdc3eb213297efb84c
lancelot <lancelot.martin@epita.fr>
2023-05-20 21:14:05 +0200

    fix

--- maps/finalMap.c
+++ maps/finalMap.c
@@ -366,14 +366,15 @@ int windowHandle(struct graph *G) {
             for(size_t i = 0; i < G->order; i++) {
                 for(size_t j = 0; j < G->inters[i].nblinks; j++) {
                     size_t end = G->inters[i].links[j].end;
-
+                    int rx1;
+                    int ry1;
                     if (G->inters[i].x > G->inters[end].x) {
-                        int rx1 = (G->inters[i].x + G->inters[end].x) / 2 + 20;
-                        int ry1 = (G->inters[i].y + G->inters[end].y) / 2 + 20;
+                        rx1 = (G->inters[i].x + G->inters[end].x) / 2 + 20;
+                        ry1 = (G->inters[i].y + G->inters[end].y) / 2 + 20;
                     }
                     else {
-                        int rx1 = (G->inters[i].x + G->inters[end].x) / 2 - 20;
-                        int ry1 = (G->inters[i].y + G->inters[end].y) / 2 - 20;
+                        rx1 = (G->inters[i].x + G->inters[end].x) / 2 - 20;
+                        ry1 = (G->inters[i].y + G->inters[end].y) / 2 - 20;
                     }
 
                     int rx = compute_pos(rx1, 0, renderX, renderY, maxX, cZoom, 0, G);
